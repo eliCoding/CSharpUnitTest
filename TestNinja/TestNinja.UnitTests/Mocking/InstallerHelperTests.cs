a70209032acf60124058af381d18f8d08ba97559
Elmira <e.amanollahi@gmail.com>
2018-09-12 13:01:32 -0700

    File Downloader

---
+++ TestNinja/TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Net;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+
+    [TestFixture]
+    class InstallerHelperTests
+    {
+        private InstallerHelper _installerHelper;
+        private Mock<IFileDownloader> _fileDownloader;
+
+        [SetUp]
+        public void setUp()
+        {
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownlodFails_ReturnFalse()
+        {
+            _fileDownloader.Setup(fd => 
+                fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+            var result = _installerHelper.DownloadInstaller("Customer", "installer");
+
+            Assert.That(result,Is.False);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownlodCompletes_ReturnTrue()
+        {
+      
+            var result = _installerHelper.DownloadInstaller("Customer", "installer");
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
